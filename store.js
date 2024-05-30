4c5f162a1a8c3e2ac39a23b33e16ca5a80ead71d
unknown <ziedzcr7@gmail.com>
2024-05-30 16:50:30 +0100

    first commit

---
+++ store.js
@@ -0,0 +1,51 @@
+class Product {
+    constructor(id,name,price){
+        this.id=id;
+        this.name=name;
+        this.price=price;
+    }
+}
+class Card_item {
+    constructor(product,quantity){
+        
+        this.product=product;
+        this.quantity=quantity;
+    }
+    totalPrice(){return this.product.price*this.quantity}
+}
+class ShoppingCard {
+    
+    constructor(){
+       
+        this.items=[];
+    }
+    TotalItems(){return `there is ${this.items.length} items in this Card`}
+    addCard(cart_item){return this.items.push(cart_item)}
+    removeCard(cart_item){
+        this.items=this.items.filter(i=>{return i.product.id != cart_item.product.id})
+    }
+    DisplayItems(){console.log(cart_global);}
+}
+
+var product1 = new Product('001','product_1','45£');
+var product2 = new Product('002','product_2','50£');
+var product3 = new Product('003','product_3','55£');
+
+var item1=new Card_item(product1,7);
+var item2=new Card_item(product2,5);
+var item3=new Card_item(product3,2);
+
+var cart_global = new ShoppingCard();
+
+
+cart_global.addCard(item1);
+cart_global.addCard(item2);
+cart_global.addCard(item3);
+
+console.log(cart_global.TotalItems());
+cart_global.removeCard(item2);
+
+
+cart_global.DisplayItems();
+
+console.log(`total price of item2 is : ${item1.totalPrice()}`);
